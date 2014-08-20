79bb35e1352b33bbf3b3091264f13f7cc46f2cce
Sebastian Schubert <schubert.seb@gmail.com>
2014-08-20 13:58:02 +0200

    Add reading of additional simulations by Sebastian

--- readSebastian.R
+++ readSebastian.R
@@ -14,63 +14,84 @@ icoord <- findlonlatindex(
 ## calculate athmospheric heights
 heights <- calcatmoheights("/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009/out02//lffd2002050100c.nc")
 
-## bulk simulation
-nurb <- read.folder.netcdf(
-    "/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009/out02/",
-    startdate, nts,
-    c("T_2M", "U_10M_AV", "V_10M_AV", "ASHFL_S", "ALHFL_S", "ALWD_S", "ALWU_S", "ASWDIR_S", "ASWDIFD_S", "ASWDIFU_S", "CLCT", "T_G", "P", "AUMFL_S", "AVMFL_S"),
-    c("at", "wvu", "wvv", "fh", "fl", "ld", "lu", "sd_dir", "sd_diff", "su", "cc", "tg", "ap", "fu", "fv"),
-    icoord,
-    maxheight=500,
-    levels = heights)
-## add simulation type
-nurb <- lapply(nurb, function(x) cbind(x, data.frame(type="bulk")))
+## read 2d fields in out02
+read2d <- function(folder) {
+    read.folder.netcdf(
+        folder,
+        startdate, nts,
+        c("T_2M", "U_10M_AV", "V_10M_AV", "ASHFL_S", "ALHFL_S", "ALWD_S", "ALWU_S", "ASWDIR_S", "ASWDIFD_S", "ASWDIFU_S", "CLCT", "T_G", "P", "AUMFL_S", "AVMFL_S"),
+        c("at", "wvu", "wvv", "fh", "fl", "ld", "lu", "sd_dir", "sd_diff", "su", "cc", "tg", "ap", "fu", "fv"),
+        icoord,
+        maxheight=500,
+        levels = heights)
+}
 
-nurb3d <- read.folder.netcdf(
-    "/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009/out01/",
-    startdate, nts,
-    c("T", "U", "V"),
-    c("at2", "wvu2", "wvv2"),
-    icoord,
-    maxheight=500,
-    levels = heights)
+## read 3d fields in out01
+read3d <- function(folder) {
+    read.folder.netcdf(
+        folder,
+        startdate, nts,
+        c("T", "U", "V"),
+        c("at2", "wvu2", "wvv2"),
+        icoord,
+        maxheight=500,
+        levels = heights)
+}
+
+## bulk simulation
+bulk <- read2d("/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009/out02/")
 ## add simulation type
-nurb3d <- lapply(nurb3d, function(x) cbind(x, data.frame(type="bulk")))
+bulk <- lapply(bulk, function(x) cbind(x, data.frame(type="bulk")))
+
+bulk3d <- read3d("/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009/out01/")
+## add simulation type
+bulk3d <- lapply(bulk3d, function(x) cbind(x, data.frame(type="bulk")))
 
 ## put both lists together
-nurb <- c(nurb, nurb3d)
+bulk <- c(bulk, bulk3d)
+
+
+## bulk5 simulation
+bulk5 <- read2d("/scratch/01/schubert/clm/output/urban_comparison/basel0.009/out02/")
+## add simulation type
+bulk5 <- lapply(bulk5, function(x) cbind(x, data.frame(type="bulk5")))
+
+bulk53d <- read3d("/scratch/01/schubert/clm/output/urban_comparison/basel0.009/out01/")
+## add simulation type
+bulk53d <- lapply(bulk53d, function(x) cbind(x, data.frame(type="bulk5")))
+
+## put both lists together
+bulk5 <- c(bulk5, bulk53d)
 
 
 ## DCEP simulation
-urb <- read.folder.netcdf(
-    "/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009_urb/out02/",
-    startdate, nts,
-    c("T_2M", "U_10M_AV", "V_10M_AV", "ASHFL_S", "ALHFL_S", "ALWD_S", "ALWU_S", "ASWDIR_S", "ASWDIFD_S", "ASWDIFU_S", "CLCT", "T_G", "P",  "AUMFL_S", "AVMFL_S"),
-    c("at", "wvu", "wvv", "fh", "fl", "ld", "lu", "sd_dir", "sd_diff", "su", "cc", "tg", "ap", "fu", "fv"),
-    icoord,
-    maxheight=500,
-    levels = heights)
+urb <- read2d("/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009_urb/out02/")
 ## add simulation type
 urb <- lapply(urb, function(x) cbind(x, data.frame(type="DCEP")))
 
-urb3d <- read.folder.netcdf(
-    "/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009_urb/out01/",
-    startdate, nts,
-    c("T", "U", "V"),
-    c("at2", "wvu2", "wvv2"),
-    icoord,
-    maxheight=500,
-    levels = heights)
+urb3d <- read3d("/iplex/01/clme/schubert/clm/output/urban_comparison/basel0.009_urb/out01/")
 ## add simulation type
 urb3d <- lapply(urb3d, function(x) cbind(x, data.frame(type="DCEP")))
 
 urb <- c(urb,urb3d)
 
+## DCEP simulation new
+urb_new <- read2d("/scratch/01/schubert/clm/output/urban_comparison/basel0.009_urb/out02/")
+## add simulation type
+urb_new <- lapply(urb_new, function(x) cbind(x, data.frame(type="DCEPnew")))
+
+urb3d_new <- read3d("/scratch/01/schubert/clm/output/urban_comparison/basel0.009_urb/out01/")
+## add simulation type
+urb3d_new <- lapply(urb3d_new, function(x) cbind(x, data.frame(type="DCEPnew")))
+
+urb_new <- c(urb_new,urb3d_new)
+
+
 ## combine both and ensure that fitting list elements are put
-## together, all variables in nurb have to be present in urb
+## together, all variables in bulk have to be present in urb
 sim <- list()
-for (vn in names(nurb)) {
-    sim[[vn]] <- rbind(nurb[[vn]], urb[[vn]])
+for (vn in names(bulk)) {
+    sim[[vn]] <- rbind(bulk[[vn]], bulk5[[vn]], urb[[vn]], urb_new[[vn]])
 }
 
 save(file="sim.Rdata", sim)
